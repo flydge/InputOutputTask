091ea978fb307ad0b17a5a9285fc850c2f446a47
flydge <followflydge@gmail.com>
2018-01-18 23:11:41 +0300

    Develop a simple console application that allowes to list all files in directory and read contents of chosen file.

---
+++ FileViewer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace InputOutputTask
+{
+    public class FileViewer
+    {
+        private string _path;
+
+        public string Path
+        {
+            get => _path;
+            set => _path = value;
+        }
+
+        public void GetMenu()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path);
+            for (int i = 0; i < directoryInfo.GetFiles().Length; i++)
+            {
+                Console.WriteLine("#{0}:{1}", i, directoryInfo.GetFiles()[i].Name);
+            }
+            Console.WriteLine("Enter the numb of the selected file.");
+            int selectedFileNumb = 0; //int.Parse(Console.ReadLine());
+
+            string prog = directoryInfo.GetFiles()[selectedFileNumb].ToString();
+            using (System.Diagnostics.Process.Start(prog))
+            {
+                //Console.Clear();
+                Console.WriteLine("selected file {0} is open.",directoryInfo.GetFiles()[selectedFileNumb].Name);
+                int time4open = int.Parse(Console.ReadLine());
+            }
+            
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace InputOutputTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "/home/flydge/snap/chromium/107/Downloads/js";
+            FileViewer fileViewer = new FileViewer();
+            fileViewer.Path = path;
+            fileViewer.GetMenu();
+           
+        }
+    }
+}
